c2c591b2c58bb916445c56b0ecdd366a2b01ce33
Zavodskyja <51874654+Zavodskyja@users.noreply.github.com>
2024-01-06 16:01:08 +0100

    artist
    potlaceni erroru kvuli scriptu

--- cypress/integration/artistPage/artistLP.ts
+++ cypress/integration/artistPage/artistLP.ts
@@ -57,8 +57,7 @@ export class ArtistPage {
 
     clickLetter()
     {
-        // 16= P
-        cy.get('.chr-scrolling-carousel > div.chr-scrolling-carousel__content > li:nth-child(16) > chr-button > a').click()
+        cy.get('.chr-scrolling-carousel > div.chr-scrolling-carousel__content > li > chr-button > a:contains("P")').click();
     }
 
 
@@ -68,11 +67,22 @@ export class ArtistPage {
     }
 
     artistClick(){
+    /* poresit proc pada na 
+    <script>
+        window.chrComponents = window.chrComponents || {};
+        window.chrComponents.tags_lpHeader = ;
+    </script>
+    Optat se Ondry + potom odstranit workaround potlacenim erroru
+    */
+
+        Cypress.on('uncaught:exception', (err, runnable) => {
+            return false
+          })
+
         cy.get('body > main > section.chr-section.pt-5.mb-12 > div > div.chr-artist-listing > section > div.chr-artist-listing__content > div > div > ol > li > a:contains("Pablo Picasso")')
         .should('exist').click();
-        cy.wait(10000);
-        cy.url().should('eq',`${Cypress.env('productUrl')}/en/artists/pablo-picasso`)
+        //cy.wait(10000);
+        cy.url().should('eq',`${Cypress.env('productUrl')}/en/artists/pablo-picasso?lotavailability=All&sortby=relevance`)
         cy.get('body > main > section > div > div > div:nth-child(1) > div > div > h1').should('exist').and('contain.text', 'Pablo Picasso');
     }
-    
     }
